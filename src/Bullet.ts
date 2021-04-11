b44a765ee3d151afbabf3c636060090a2faa28ac
Mike Precup <mike@precup.email>
2021-04-11 03:54:04 -0700

    Art updates, stages, misc

--- src/Bullet.ts
+++ src/Bullet.ts
@@ -1,47 +1,80 @@
-import { Ship } from "src/Ship"
+import { Ship } from "src/Ship";
+
+const BULLET_COLORS = [
+  new Color(193 / 256.0, 48 / 256.0, 28 / 256.0),
+  new Color(201 / 256.0, 97 / 256.0, 18 / 256.0),
+  new Color(196 / 256.0, 167 / 256.0, 5 / 256.0),
+  new Color(23 / 256.0, 114 / 256.0, 69 / 256.0),
+  new Color(46 / 256.0, 87 / 256.0, 147 / 256.0),
+  new Color(75 / 256.0, 40 / 256.0, 130 / 256.0),
+];
+
+const NUM_LIVES = 6;
 
 export class Bullet extends Area2D {
-  speed: float = 100.0
-  bounds: Rect2
-  x_d: float
-  y_d: float
-  alive_counter = 0
+  @exports
+  lives = 3;
+  speed: float = 350.0;
+  bounds: Rect2;
+  x_d: float;
+  y_d: float;
+  safe_spawn_buffer = 0.5;
+  horiz_lives = this.lives;
+  vert_lives = this.lives;
 
   constructor() {
-    super()
+    super();
 
-    this.bounds = this.get_viewport_rect()
+    this.bounds = this.get_viewport_rect();
 
-    this.x_d = cos(this.rotation) * this.speed
-    this.y_d = sin(this.rotation) * this.speed
+    this.x_d = cos(this.rotation) * this.speed;
+    this.y_d = sin(this.rotation) * this.speed;
+    this.horiz_lives = this.lives;
+    this.vert_lives = this.lives;
 
-    this.connect("body_entered", this, "on_body_enter")
+    this.connect("body_entered", this, "on_body_enter");
   }
 
   on_body_enter(body: Node2D) {
-    // alive_counter prevents newly spawned bullets from immediately killing you
+    // safe_spawn_buffer prevents newly spawned bullets from immediately killing you
 
-    if (body instanceof Ship && this.alive_counter > 30) {
-      body.die()
+    if (body instanceof Ship && this.safe_spawn_buffer <= 0) {
+      body.die();
     }
   }
 
   _process(delta: float) {
-    this.alive_counter++
+    this.safe_spawn_buffer -= delta;
 
-    this.position.x += this.x_d * delta
-    this.position.y += this.y_d * delta
+    this.position.x += this.x_d * delta;
+    this.position.y += this.y_d * delta;
 
     if (this.x_d < 0 && this.position.x < this.bounds.position.x) {
-      this.position.x += this.bounds.size.x
+      this.position.x += this.bounds.size.x;
+      this.horiz_lives--;
     } else if (this.x_d > 0 && this.position.x > this.bounds.end.x) {
-      this.position.x -= this.bounds.size.x
+      this.position.x -= this.bounds.size.x;
+      this.horiz_lives--;
     }
 
     if (this.y_d < 0 && this.position.y < this.bounds.position.y) {
-      this.position.y += this.bounds.size.y
+      this.position.y += this.bounds.size.y;
+      this.vert_lives--;
     } else if (this.y_d > 0 && this.position.y > this.bounds.end.y) {
-      this.position.y -= this.bounds.size.y
+      this.position.y -= this.bounds.size.y;
+      this.vert_lives--;
+    }
+
+    const min_lives = min(this.horiz_lives, this.vert_lives);
+    if (min_lives <= 0) {
+      this.queue_free();
+    }
+
+    // Leave the bullet white on spawning to indicate it's safe
+    if (this.safe_spawn_buffer <= 0) {
+      this.get_node("Sprite").set_modulate(
+        BULLET_COLORS[max(0, min(BULLET_COLORS.size(), min_lives) - 1)]
+      );
     }
   }
 }
--- src/Ship.ts
+++ src/Ship.ts
@@ -2,12 +2,13 @@ import { Gun } from "src/Gun";
 
 export class Ship extends KinematicBody2D {
   velocity = new Vector2(0, 0);
-  max_velocity = 400.0;
-  accel = 60000.0;
-  deaccel = 60000.0;
+  max_velocity = 350.0;
+  accel = 55000.0;
+  deaccel = 55000.0;
   you_die_label = this.get_node("/root/RootNode/YouDieLabel") as Label;
   final_score_label = this.get_node("/root/RootNode/FinalScoreLabel") as Label;
   gun = this.get_node("/root/RootNode/Gun") as Gun;
+  bounds: Rect2;
 
   w_held: bool = false;
   a_held: bool = false;
@@ -19,9 +20,10 @@ export class Ship extends KinematicBody2D {
     this.position.x = 200;
     this.you_die_label.visible = false;
     this.final_score_label.visible = false;
+    this.bounds = this.get_viewport_rect();
   }
 
-  _process(delta: float) {
+  _physics_process(delta: float) {
     const move_left =
       Input.is_key_pressed(KeyList.KEY_A) && (this.a_held || !this.d_held);
     const move_right = Input.is_key_pressed(KeyList.KEY_D) && !this.a_held;
@@ -73,6 +75,17 @@ export class Ship extends KinematicBody2D {
       );
     } else {
       this.move_and_slide(this.velocity);
+    }
+
+    if (this.position.x < 0 && move_left) {
+      this.position = this.position.add(new Vector2(this.bounds.size.x, 0));
+    } else if (this.position.x > this.bounds.size.x && move_right) {
+      this.position = this.position.sub(new Vector2(this.bounds.size.x, 0));
+    }
+    if (this.position.y < 0 && move_up) {
+      this.position = this.position.add(new Vector2(0, this.bounds.size.y));
+    } else if (this.position.y > this.bounds.size.y && move_down) {
+      this.position = this.position.sub(new Vector2(0, this.bounds.size.y));
     }
   }
 
